00e478c47f0327587abe09f1450c7bdd10b77360
fibonacci998 <fibonacci998@gmai.com>
2021-03-16 10:08:11 +0700

    update helm chart prerequisite

--- using-sh/1.prerequisite/deploy.sh
+++ using-sh/1.prerequisite/deploy.sh
@@ -1,4 +1,4 @@
 kubectl create namespace graylog
-helm install --namespace "graylog" -n "mongodb" stable/mongodb-replicaset -f mongo-values.yaml
-helm install --namespace "graylog" -n "elasticsearch" stable/elasticsearch -f elastic-values.yaml
+helm install --namespace "graylog" -n "mongodb" ./mongodb-replicaset -f mongo-values.yaml
+helm install --namespace "graylog" -n "elasticsearch" ./elasticsearch -f elastic-values.yaml
 
